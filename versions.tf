d3e36ce2eaa072f48f28bb36e929150355f2a8b2
Wei Jing <hyd.wei.jing@gmail.com>
2025-08-20 23:23:28 +1200

    another init

--- versions.tf
+++ versions.tf
@@ -9,5 +9,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region                  = "us-east-1"
+  shared_credentials_file = "%USERPROFILE%\\.aws\\credentials"
+  profile                 = "vscode"
 }
