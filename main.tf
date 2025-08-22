33a2bf11fb29151a9652f23f71aa858435fa84ea
Wei Jing <jaychou123103@hotmail.com>
2025-08-22 23:08:42 +1200

    add subnet and igw

--- main.tf
+++ main.tf
@@ -7,3 +7,22 @@ resource "aws_vpc" "terraform_test_vpc" {
     Name = "dev"
   }
 }
+
+resource "aws_subnet" "terraform_test_subnet" {
+  vpc_id                  = aws_vpc.terraform_test_vpc.id
+  cidr_block              = "10.123.0.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "dev-public"
+  }
+}
+
+resource "aws_internet_gateway" "terraform_test_internet_gateway" {
+  vpc_id = aws_vpc.terraform_test_vpc.id
+
+  tags = {
+    Name = "dev-igw"
+  }
+}
